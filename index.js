0c6ce0bcd82ae620eff07e51ed49d0cf8d762f1b
omar ahmed <omar.ahmed.ibrahiem2@gmail.com>
2026-04-17 02:17:41 +0200

    fixes

--- index.js
+++ index.js
@@ -24,6 +24,7 @@ app.use(helmet({
     directives: {
       defaultSrc: ["'self'"],
       scriptSrc: ["'self'", "'unsafe-inline'", "'unsafe-eval'"],
+      scriptSrcAttr: ["'unsafe-inline'"],
       styleSrc: ["'self'", "'unsafe-inline'", 'https://fonts.googleapis.com'],
       fontSrc: ["'self'", 'https://fonts.gstatic.com'],
       imgSrc: ["'self'", 'data:', 'https:'],
